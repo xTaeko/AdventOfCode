63af1b8e323212c41cd97c5ff65b7e8c9e7f3d35
Vanessa <vanessa.speeth@smail.inf.h-brs.de>
2021-12-06 18:58:53 +0100

    Day 4 part 2

--- Day4/DayFourChallenge.py
+++ Day4/DayFourChallenge.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 def part_one():
-    list_solved = []
     with open("Input.txt") as file:
         numbers_drawn = file.readline().split(",")
 
@@ -27,17 +26,50 @@ def part_one():
                     board[marker_board == True] = 0
                     sum_unmarked = np.sum(board)
                     result = sum_unmarked * int(number)
-                    print(result)
-                    exit()
+                    return result
                 counter += 1
 
 
-part_one()
+result_part_one = part_one()
+print("The result of the first winning board is: ", result_part_one)
 
 
 def part_two():
-    print("TWO")
-    # List of boards (100) - delete every that wins
+    with open("Input.txt") as file:
+        numbers_drawn = file.readline().split(",")
+
+        data = np.loadtxt("Input.txt", skiprows=2)
+        list_of_boards = np.array(np.split(data, len(data) / 5))
+        list_of_markers = np.full_like(list_of_boards, False, dtype=bool)
+
+        last_winning_board = np.zeros((5, 5))
+        last_winning_number = -1
+        last_winning_marker_board = np.full_like(last_winning_board, False, dtype=bool)
+        # Boolean array indexing: https://numpy.org/doc/stable/reference/arrays.indexing.html#boolean-array-indexing
+        for number in numbers_drawn:
+
+            list_of_markers[list_of_boards == int(number)] = True
+            counter = 0
+            for marker_board in list_of_markers:
+                solved_horizontal = np.all(marker_board, axis=1)
+                solved_vertical = np.all(marker_board, axis=0)
+                is_solved_horizontal = np.any(solved_horizontal, axis=0)
+                is_solved_vertical = np.any(solved_vertical, axis=0)
+                if (is_solved_horizontal or is_solved_vertical):
+                    # Create copys to avoid call by reference, when reference is set to None
+                    last_winning_board = np.copy(list_of_boards[counter])
+                    last_winning_number = number
+                    last_winning_marker_board = np.copy(marker_board)
+                    # Set all solved boards to None, to stop the board from calculating more.
+                    list_of_boards[counter] = None
+                    list_of_markers[counter] = None
+                counter += 1
+
+        last_winning_board[last_winning_marker_board == True] = 0
+        sum_unmarked = np.sum(last_winning_board)
+        result = sum_unmarked * int(last_winning_number)
+        return result
 
 
-part_two()
+result_part_two = part_two()
+print("The score of the last winning board is: ", result_part_two)
